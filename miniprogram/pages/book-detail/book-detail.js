41c64d89481ec4440eaf56911a3459660d639c35
huhai <984736323@qq.com>
2019-12-12 14:01:10 +0800

    yiyi

--- miniprogram/pages/book-detail/book-detail.js
+++ miniprogram/pages/book-detail/book-detail.js
@@ -24,7 +24,7 @@ Page({
       name: 'book',
       data: {
         $url: 'bookDetai',
-        bid:options.bid
+        bid: options.bid
       }
     }).then((res) => {
       this.setData({
@@ -32,75 +32,6 @@ Page({
       })
       wx.hideLoading()
     })
-
-
-    // const bid = options.bid
-    // const detail = bookModel.getDetail(bid)
-    // const comments = bookModel.getComments(bid)
-    // const likeStatus = bookModel.getLikeStatus(bid)
-
-
-    // Promise.all([detail, comments, likeStatus])
-    //   .then(res => {
-    //     this.setData({
-    //       book: res[0],
-    //       comments: res[1].comments,
-    //       likeStatus: res[2].like_status,
-    //       likeCount: res[2].fav_nums
-    //     })
-    //     wx.hideLoading()
-    //   })
-  },
-
-  onLike(event) {
-    const like_or_cancel = event.detail.behavior
-    likeModel.like(like_or_cancel, this.data.book.id, 400)
-  },
-
-  onFakePost(event) {
-    this.setData({
-      posting: true
-    })
-  },
-
-  onCancel(event) {
-    this.setData({
-      posting: false
-    })
-  },
-
-  onPost(event) {
-    const comment = event.detail.text || event.detail.value
-
-    if (!comment) {
-      return
-    }
-
-    if (comment.length > 12) {
-      wx.showToast({
-        title: '短评最多12个字',
-        icon: 'none'
-      })
-      return
-    }
-
-    bookModel.postComment(this.data.book.id, comment)
-      .then(res => {
-        wx.showToast({
-          title: '+ 1',
-          icon: "none"
-        })
-
-        this.data.comments.unshift({
-          content: comment,
-          nums: 1
-        })
-
-        this.setData({
-          comments: this.data.comments,
-          posting: false
-        })
-      })
   },
 
   /**
